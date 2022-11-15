915306cd062793c7bc3c1023737e78fd67b5e1ab
Cisse Mertens <91118379+CisseM03@users.noreply.github.com>
2022-11-15 22:16:14 +0100

    Update faker_project.py

--- app/faker_project.py
+++ app/faker_project.py
@@ -6,7 +6,11 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "*"
+    "http://localhost/",
+    "http://localhost:8080/",
+    "https://localhost.tiangolo.com/",
+    "http://127.0.0.1:5500/",
+    "https://cissem03.github.io"
 ]
 
 app.add_middleware(
