1abe04635c571b28841d8ad36e974f9dd778e332
Cisse Mertens <91118379+CisseM03@users.noreply.github.com>
2022-11-15 20:39:29 +0100

    Update faker_project.py

--- app/faker_project.py
+++ app/faker_project.py
@@ -1,11 +1,27 @@
 from faker import Faker
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost/",
+    "http://localhost:8080/",
+    "https://localhost.tiangolo.com/",
+    "http://127.0.0.1:5500/",
+    "https://CisseM03.github.io/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 name = "identify yourself!"
-
-
 
 class Person(BaseModel):
     first_name: str
